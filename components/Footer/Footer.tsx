0dd1194952638cdbbc58f4cf36fbbdf56399dac4
Anastasiia Chaplyhina <chaplyhina.anastasiia@gmail.com>
2025-12-14 18:40:33 +0100

    Add missing Header and Footer components

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import css from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={css.footer}>
+      <div className={css.content}>
+        <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
