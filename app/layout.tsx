168addb9f437a2c2b2bf41a07178259f1892a60e
Anastasiia Chaplyhina <chaplyhina.anastasiia@gmail.com>
2025-12-16 17:22:39 +0100

    Fix main lock

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,43 @@
-// app/layout.tsx
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-import Providers from "@/components/Providers/Providers"; 
+import Providers from "@/components/Providers/Providers";
 
-const inter = Inter({ subsets: ["latin"] });
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export const metadata: Metadata = {
   title: "NoteHub",
-  description: "Manage your notes efficiently",
+  description: "Manage your notes easily",
 };
 
 export default function RootLayout({
   children,
+  modal, 
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode; 
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Providers>
-          <div className="container">
-            <Header />
-            <main className="main">{children}</main>
-            <Footer />
-          </div>
+          <Header />
+          <main>
+            {children}
+            {modal} 
+          </main>
+          <Footer />
         </Providers>
       </body>
     </html>
