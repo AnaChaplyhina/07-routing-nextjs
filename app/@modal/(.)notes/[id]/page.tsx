46577c4dd9be48307765f01caee3dbe4fb9bf62b
Anastasiia Chaplyhina <chaplyhina.anastasiia@gmail.com>
2025-12-16 18:09:15 +0100

    Add missing NoteDetails.client.tsx and connect it to page

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,18 +1,22 @@
-import Modal from '@/components/Modal/Modal';
-import NotePreview from '@/components/NotePreview/NotePreview';
 import { getNoteById } from '@/lib/api/notes';
+import Link from 'next/link';
+import NoteDetailsClient from './NoteDetails.client';
 
 interface PageProps {
   params: Promise<{ id: string }>;
 }
 
-export default async function InterceptedNotePage({ params }: PageProps) {
+export default async function Page({ params }: PageProps) {
   const resolvedParams = await params;
   const note = await getNoteById(resolvedParams.id);
 
   return (
-    <Modal>
-      <NotePreview note={note} />
-    </Modal>
+    <div style={{ maxWidth: '800px', margin: '0 auto', padding: '20px' }}>
+      <Link href="/notes/filter/all" style={{ marginBottom: '20px', display: 'inline-block' }}>
+        ← Back to list
+      </Link>
+      
+      <NoteDetailsClient note={note} />
+    </div>
   );
 }
